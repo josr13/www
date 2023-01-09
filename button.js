8d80e8bfd7774e9268cb6ff79bb539a2171c40a3
hiawøg <joseph-l-r-r@hotmail.com>
2023-01-08 20:25:34 -0500

    Added contact form

---
+++ button.js
@@ -0,0 +1,5 @@
+function doStuff() {
+    document.getElementById("name-button").innerHTML = "BUTTON";
+    alert("Button name has been updated!")
+    // tiene que ser el mismo id en una sola funcion
+}
